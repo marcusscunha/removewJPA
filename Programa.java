b9f510c9395d1b8bbd81ec2c8c8ef51de70e1578
Marcus Vitor da Cunha Nascimento <106033361+marcusscunha@users.noreply.github.com>
2023-04-14 11:12:52 -0300

    Add files via upload

---
+++ Programa.java
@@ -0,0 +1,29 @@
+package programa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("dominio");
+		EntityManager em = emf.createEntityManager();
+	
+	
+		Pessoa p = em.find(Pessoa.class,3);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		System.out.println("Done!");
+		em.close();
+		emf.close();
+	
+	}
+
+}
